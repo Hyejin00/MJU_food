89af92c322c561943e3e3195cf2ce77b61fc77c4
Hyejin00 <svivs137@naver.com>
2019-06-05 00:47:46 +0900

    itme

--- app/src/main/java/com/project/hyejin/mju_food/America.java
+++ app/src/main/java/com/project/hyejin/mju_food/America.java
@@ -1,15 +1,18 @@
 package com.project.hyejin.mju_food;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class America extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private String[] myDataset;
+    private Intent intent_url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,38 @@ public class America extends AppCompatActivity {
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+        ItemClickSupport.addTo(recyclerView).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+            @Override
+            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                intent_url = new Intent(getApplicationContext(),Web.class);
+                switch (position){
+                    case 0:
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=1018893325");
+                        break;
+                    case 1:
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=11839785");
+                        break;
+                    case 2:
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?entry=plt&id=36936507&query=%EB%AF%B8%EC%8A%A4%ED%84%B0%ED%94%BC%EC%9E%90%20%EB%AA%85%EC%A7%80%EB%8C%80%EC%A0%90");
+                        break;
+                    case 3:
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?entry=plt&id=31161270&query=%EC%A3%BC%EC%9D%B8%EB%B0%B1%ED%8C%8C%EC%8A%A4%ED%83%80");
+                        break;
+                    case 4:
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?entry=plt&id=1899165929&query=%EC%9D%80%ED%99%94%EC%88%98%EC%8B%9D%EB%8B%B9%20%EB%AA%85%EC%A7%80%EB%8C%80%EC%A0%90");
+                        break;
+                    case 5:
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=20886510");
+                        break;
+                    case 6:
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?entry=plt&id=36324274&query=%EC%B0%A8%EC%8A%A4%EC%BF%A1");
+                        break;
+                }
+                startActivity(intent_url);
+
+            }
+        });
 
         // specify an adapter
         mAdapter = new MyAdapter(myDataset);
--- app/src/main/java/com/project/hyejin/mju_food/Japan.java
+++ app/src/main/java/com/project/hyejin/mju_food/Japan.java
@@ -20,7 +20,7 @@ public class Japan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.japan);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        myDataset = new String[]{"유월의 살구나무","Kamome","인라면","원펀맨","가타쯔무리","소소카레","스시하나에","홍대무라","나라비","브로콜리 식당"};
+        myDataset = new String[]{"유월의 살구나무","인라면","원펀멘","가타쯔무리","소소카레","스시하나에","홍대무라","나라비","브로콜리 식당"};
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -36,31 +36,31 @@ public class Japan extends AppCompatActivity {
                 intent_url = new Intent(getApplicationContext(),Web.class);
                 switch (position){
                     case 0:
-                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=154308881");
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?entry=plt&id=33452630&query=%EC%9C%A0%EC%9B%94%EC%9D%98%20%EC%82%B4%EA%B5%AC%EB%82%98%EB%AC%B4");
                         break;
                     case 1:
-                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=1220114339");
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?entry=plt&id=34678368&query=%EC%9D%B8%EB%9D%BC%EB%A9%B4");
                         break;
                     case 2:
-                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=31222381");
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=1149275842");
                         break;
                     case 3:
-                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=1102427977");
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?entry=plt&id=36351814&query=%EA%B0%80%ED%83%80%EC%AF%94%EB%AC%B4%EB%A6%AC");
                         break;
                     case 4:
-                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=1135410066");
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?entry=plt&id=1306698918&query=%EC%86%8C%EC%86%8C%EC%B9%B4%EB%A0%88");
                         break;
                     case 5:
-                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=1244111561");
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?entry=plt&id=38299775&query=%EC%8A%A4%EC%8B%9C%ED%95%98%EB%82%98%EC%97%90");
                         break;
                     case 6:
-                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=930079065");
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?entry=plt&id=38329482&query=%ED%99%8D%EB%8C%80%EB%AC%B4%EB%9D%BC");
                         break;
                     case 7:
-                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=20851938");
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=33876033");
                         break;
                     case 8:
-                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?id=20033096");
+                        intent_url.putExtra("Url","https://store.naver.com/restaurants/detail?entry=plt&id=38009356&query=%EB%B8%8C%EB%A1%9C%EC%BD%9C%EB%A6%AC%EC%8B%9D%EB%8B%B9");
                         break;
                 }
                 startActivity(intent_url);
